41b6447ed96c97e3eca1edb2fe63ad06df9eacd3
Rama-Qandeel <rama.qndeel@gmail.com>
2020-10-31 16:47:27 +0200

    updata

--- react/my-app/src/components/Students.js
+++ react/my-app/src/components/Students.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react'
 
 export default class Students extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+         students:[{id:"",
+        username:'',
+        class:'',
+        email:''
+        }]
+      }
+    }
+  
+  
     render() {
-       
+        let header = Object.keys(this.state.students[0])
+        let key=header.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+         })
+
+
    const students= this.props.student.map((student,i)=>{
-        return <p>{i+1}-{student.username}</p>
+        return ( <tr key={student.id}>
+            <td>{i+1}</td>
+            <td>{student.username}</td>
+          <td>{student.class}</td>
+          <td>{student.email}</td>
+            {/* <td>{teacher.roleid}</td> */}
+        </tr>)
     })
        
         return (
             
             
             <div>
-               {students}
+              <table id='students'>
+               <tbody>
+               <tr>{key}</tr>
+                  {students}
+               </tbody>
+               </table>
             </div>
         )
     }
--- react/my-app/src/components/Teachers.js
+++ react/my-app/src/components/Teachers.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react'
-
+import "./App.css"
 export default class Teachers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+         teachers:[{id:"",
+        username:'',
+        material:'',
+        email:''
+        }]
+      }
+    }
+   
+   
+   
     render() {
-       
+        let header = Object.keys(this.state.teachers[0])
+    //    console.log('header : ',header);
+   
+     let key=header.map((key, index) => {
+           return <th key={index}>{key.toUpperCase()}</th>
+        })
+    //  console.log('h : ',r);
+     
    const teachers= this.props.teachers.map((teacher,i)=>{
-   return <p> {i+1}-{teacher.username}</p>
+   
+    return (
+       <tr key={teacher.id}>
+           <td>{i+1}</td>
+           <td>{teacher.username}</td>
+         <td>{teacher.material}</td>
+         <td>{teacher.email}</td>
+           {/* <td>{teacher.roleid}</td> */}
+       </tr>
+   )
     })
-       
+ 
         return (
             
             
             <div>
-               {teachers}
+               <table id='students'>
+               <tbody>
+               <tr>{key}</tr>
+                  {teachers}
+               </tbody>
+               </table>
             </div>
         )
     }
